abe3d358e73e044734d215c429bee2f3471f9f57
zezotaha1 <101072083+zezotaha1@users.noreply.github.com>
2025-04-18 17:37:06 +0200

    Add AppUser And finish the arc

---
+++ Core/Models/AppUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Enums;
+using Microsoft.AspNetCore.Identity;
+
+namespace Core.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public string ProfilePicture { get; set; }
+
+        public double? Lat { get; set; }
+        public double? Lng { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
