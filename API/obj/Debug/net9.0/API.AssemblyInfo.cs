aea6996eb2135424613bd65c97bc161248abe2c6
MahmoodAyman <mahmoudayman1542@gmail.com>
2025-04-18 14:49:17 +0200

    Remove appsettings.json from the whole git history

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5effb7341fe61b2b0bc5de1accb72566581c2de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+780b956c930486be085d862915b14b36cbf3986d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
